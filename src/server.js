0f4f0bbcb9a649b6e6a92d16ec04c1b5bcf70bb4
manohySr <manohyrajaonah@gmail.com>
2024-06-24 08:22:41 -0300

    feat: separate database connection logic

--- src/server.js
+++ src/server.js
@@ -1,36 +1,14 @@
 const { createServer } = require("http");
 require("dotenv").config();
-const mongoose = require("mongoose");
+
 const app = require("./app.js");
+const { mongoConnect } = require("./services/mongo.js");
 const { loadPlanetsData } = require("./models/planets/planets.model.js");
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGO_URL;
-
-const clientOptions = {
-  serverApi: { version: "1", strict: true, deprecationErrors: true },
-};
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(MONGO_URL, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-
-    mongoose.connection.once("open", () => {
-      console.log("MongoDB connecion ready!");
-    });
-
-    mongoose.connection.on("error", (err) => {
-      console.error(err);
-    });
-  } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error}`);
-    process.exit(1); // Exit process with failure
-  }
-}
-run().catch(console.dir);
 
 const startServer = async () => {
+  await mongoConnect();
   await loadPlanetsData();
 
   const server = createServer(app);
