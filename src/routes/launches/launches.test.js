7ec59d4fbca128dd20e0bc5de4bf098777c589c3
manohySr <manohyrajaonah@gmail.com>
2024-06-24 08:50:12 -0300

    test: Add integration tests

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -1,64 +1,78 @@
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo.js");
 const app = require("./../../app.js");
 const request = require("supertest");
 
-describe("Test GET /lauch", () => {
-  test("It should response 200 success", async () => {
-    await request(app).get("/launch").expect(200);
+describe("LAUNCHES API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
-});
-
-describe("Test POST /launch", () => {
-  const completeDataLaunch = {
-    mission: "Kepler Exploration X",
-    rocket: "Explorer IS1",
-    launchDate: "January 20, 2030",
-    target: "Kepler-442 b",
-  };
-  const dataLaunchWithoutDate = {
-    mission: "Kepler Exploration X",
-    rocket: "Explorer IS1",
-    target: "Kepler-442 b",
-  };
-  const dataLaunchInvalidDate = {
-    mission: "Kepler Exploration X",
-    rocket: "Explorer IS1",
-    launchDate: "groot",
-    target: "Kepler-442 b",
-  };
-
-  test("It should response 201 success", async () => {
-    const response = await request(app)
-      .post("/launch")
-      .send(completeDataLaunch)
-      .expect("Content-Type", /json/)
-      .expect(201);
-    const requestLaunchDate = new Date(completeDataLaunch.launchDate).valueOf();
-    const responseLaunchDate = new Date(response.body.data.launchDate).valueOf();
-    expect(responseLaunchDate).toBe(requestLaunchDate);
-    expect(response.body.data).toMatchObject(dataLaunchWithoutDate);
+  afterAll(async () => {
+    await mongoDisconnect();
   });
-  test("It should catch missing error", async () => {
-    const response = await request(app)
-      .post("/launch")
-      .send(dataLaunchWithoutDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
-    expect(response.body).toStrictEqual({
-      ok: false,
-      error:
-        "launch.mission | launch.rocket | launch.launchDate | launch.target should not be void",
+
+  describe("Test GET /lauch", () => {
+    test("It should response 200 success", async () => {
+      await request(app).get("/launch").expect(200);
     });
   });
 
-  test("It should catch invalid date error", async () => {
-    const response = await request(app)
-      .post("/launch")
-      .send(dataLaunchInvalidDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
-    expect(response.body).toStrictEqual({
-      ok: false,
-      error: "date format error",
+  describe("Test POST /launch", () => {
+    const completeDataLaunch = {
+      mission: "Kepler Exploration X",
+      rocket: "Explorer IS1",
+      launchDate: "January 20, 2030",
+      target: "Kepler-442 b",
+    };
+    const dataLaunchWithoutDate = {
+      mission: "Kepler Exploration X",
+      rocket: "Explorer IS1",
+      target: "Kepler-442 b",
+    };
+    const dataLaunchInvalidDate = {
+      mission: "Kepler Exploration X",
+      rocket: "Explorer IS1",
+      launchDate: "groot",
+      target: "Kepler-442 b",
+    };
+
+    test("It should response 201 success", async () => {
+      const response = await request(app)
+        .post("/launch")
+        .send(completeDataLaunch)
+        .expect("Content-Type", /json/)
+        .expect(201);
+      const requestLaunchDate = new Date(
+        completeDataLaunch.launchDate
+      ).valueOf();
+      const responseLaunchDate = new Date(
+        response.body.data.launchDate
+      ).valueOf();
+      expect(responseLaunchDate).toBe(requestLaunchDate);
+      expect(response.body.data).toMatchObject(dataLaunchWithoutDate);
+    });
+    test("It should catch missing error", async () => {
+      const response = await request(app)
+        .post("/launch")
+        .send(dataLaunchWithoutDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+      expect(response.body).toStrictEqual({
+        ok: false,
+        error:
+          "launch.mission | launch.rocket | launch.launchDate | launch.target should not be void",
+      });
+    });
+
+    test("It should catch invalid date error", async () => {
+      const response = await request(app)
+        .post("/launch")
+        .send(dataLaunchInvalidDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+      expect(response.body).toStrictEqual({
+        ok: false,
+        error: "date format error",
+      });
     });
   });
 });
--- src/routes/planets/planets.test.js
+++ src/routes/planets/planets.test.js
@@ -1,8 +1,18 @@
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo.js");
 const app = require("./../../app.js");
 const request = require("supertest");
 
-describe("Test GET /planet", () => {
-  test("It should response 200 success", async () => {
-    await request(app).get("/planet").expect(200);
+describe("PLANETS API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
+  });
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
+
+  describe("Test GET /planet", () => {
+    test("It should response 200 success", async () => {
+      await request(app).get("/planet").expect(200);
+    });
   });
 });
